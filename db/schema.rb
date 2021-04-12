57f65f9cbc8daf234f9edb1adc3ffc173f8da8a4
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-12 19:02:49 +0200

    e

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_165156) do
+ActiveRecord::Schema.define(version: 2021_04_12_170148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "stars"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "chef_name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
